e25f738fc5375e402281c126e2e64aa6bfc303d1
Jesús Sánchez <jmsr060391@gmail.com>
2024-05-01 08:08:15 -0500

    terminado2

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -6,27 +6,27 @@ import './styles/HomePage.css'
 
 const HomePage = () => {
 
-const dispatch = useDispatch();
+  const dispatch = useDispatch()
 
-const navigate  = useNavigate();
+  const navigate = useNavigate()
 
-const textInput = useRef();
+  const textInput = useRef()
 
-const handleSubmit = (event) => {
-event.preventDefault();
-dispatch(setTrainer(textInput.current.value.trim()));
-textInput.current.value='';
-navigate('/pokedex');
-}
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    dispatch(setTrainer(textInput.current.value.trim()))
+    textInput.current.value = ''
+    navigate('/pokedex')
+  }
 
   return (
     <div className='homepage'>
-      <img className='homepage__img' src="\public\assets\98042af437fdff212d3259040db2e2db.png" alt=""/>
+      <img className='homepage__img' src="\assets\98042af437fdff212d3259040db2e2db.png" alt="" />
       <h1 className='homepage__title'>Hi Trainer!</h1>
       <h2 className='homepage__title2'>to start give me your name</h2>
-      <form  className='homepage__form' onSubmit={handleSubmit}>
-        <input className='homepage__input' ref={textInput} type="text" placeholder='Your name... '/>
-        <button className='homepage__btn'>Start</button>        
+      <form className='homepage__form' onSubmit={handleSubmit}>
+        <input className='homepage__input' ref={textInput} type="text" placeholder='Your name... ' />
+        <button className='homepage__btn'>Start</button>
       </form>
       <footer className='homepage__footer'>
       </footer>
